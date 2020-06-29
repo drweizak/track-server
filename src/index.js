c21ab461f6ff467d5d3b7eee1150efc893dbe59b
Andre Ferreira <andreferreira.coder@gmail.com>
2020-06-29 11:53:29 +0200

    tracks routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 require('./models/User');
+require('./models/Track');
 
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = 'mongodb+srv://admin:pgVW517NgqBkAHSc@cluster0-ljtgc.mongodb.net/<dbname>?retryWrites=true&w=majority'
 mongoose.connect(mongoUri, {
